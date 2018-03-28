2ac838edec3d0d23c9a7c867252654e8dd4f61da
Siddhartha Basu <biosidd@gmail.com>
2018-03-28 11:18:04 +0000

    Renamed pubsub to message

--- message/nats/req.go
+++ message/nats/req.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"time"
 
+	"github.com/dictyBase/modware-identity/message"
 	gnats "github.com/nats-io/go-nats"
 
-	"github.com/dictyBase/apihelpers/pubsub"
 	"github.com/dictyBase/go-genproto/dictybaseapis/api/jsonapi"
 	"github.com/nats-io/go-nats/encoders/protobuf"
 )
@@ -15,7 +15,7 @@ type natsRequest struct {
 	econn *gnats.Conn
 }
 
-func NewRequest(url string, options ...gnats.Option) (pubsub.Request, error) {
+func NewRequest(url string, options ...gnats.Option) (message.Request, error) {
 	nc, err := gnats.Connect(url, options...)
 	if err != nil {
 		return &natsRequest{}, err
@@ -27,14 +27,14 @@ func NewRequest(url string, options ...gnats.Option) (pubsub.Request, error) {
 	return &natsRequest{econn: ec}, nil
 }
 
-func (n *natsRequest) Request(subj string, r *jsonapi.IdRequest, timeout time.Duration) (*pubsub.Reply, error) {
-	reply := &pubsub.Reply{}
+func (n *natsRequest) Request(subj string, r *jsonapi.IdRequest, timeout time.Duration) (*message.Reply, error) {
+	reply := &message.Reply{}
 	err := n.econn.Request(subj, r, reply, timeout)
 	return reply, err
 }
 
-func (n *natsRequest) RequestWithContext(ctx context.Context, subj string, r *jsonapi.IdRequest) (*pubsub.Reply, error) {
-	reply := &pubsub.Reply{}
+func (n *natsRequest) RequestWithContext(ctx context.Context, subj string, r *jsonapi.IdRequest) (*message.Reply, error) {
+	reply := &message.Reply{}
 	err := n.econn.RequestWithContext(ctx, subj, r, reply)
 	return reply, err
 }
