c5f725b16bb0bf581997c8204b8426b30f4db473
Siddhartha Basu <biosidd@gmail.com>
2018-04-08 11:23:51 +0000

    Use pubsub.IdRequest definition for sending user request for messaging

--- message/message.go
+++ message/message.go
@@ -8,6 +8,6 @@ import (
 )
 
 type Request interface {
-	Request(string, *pubsub.IdRequest, time.Duration) (*pubsub.UserReply, error)
-	RequestWithContext(context.Context, string, *pubsub.IdRequest) (*pubsub.UserReply, error)
+	UserRequest(string, *pubsub.IdRequest, time.Duration) (*pubsub.UserReply, error)
+	UserRequestWithContext(context.Context, string, *pubsub.IdRequest) (*pubsub.UserReply, error)
 }
