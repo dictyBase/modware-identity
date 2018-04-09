75df32548efe9788a53aa75512cf062d40e10e22
Siddhartha Basu <biosidd@gmail.com>
2018-04-09 20:26:11 +0000

    Added reply interface

--- message/message.go
+++ message/message.go
@@ -7,7 +7,21 @@ import (
 	"github.com/dictyBase/go-genproto/dictybaseapis/pubsub"
 )
 
+type IdentityClient interface {
+	Get(int64) (*idenity.Identity, error)
+	Delete(int64) (bool, error)
+	Exist(int64) (bool, error)
+}
+
 type Request interface {
 	UserRequest(string, *pubsub.IdRequest, time.Duration) (*pubsub.UserReply, error)
 	UserRequestWithContext(context.Context, string, *pubsub.IdRequest) (*pubsub.UserReply, error)
+}
+
+type ReplyFn func(string, IdentityClient, *pubsub.IdRequest) *pubsub.IdentityReply
+
+type Reply interface {
+	Publish(string, *pubsub.IdentityReply)
+	Start(string, IdentityClient, ReplyFn) error
+	Stop() error
 }
