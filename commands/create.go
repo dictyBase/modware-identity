e1f98fd1fa00f03381aae5b236323a218f212acf
Siddhartha Basu <biosidd@gmail.com>
2018-05-18 10:39:03 +0000

    Added implementation for create-identity command

---
+++ commands/create.go
@@ -0,0 +1,77 @@
+package commands
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/dictyBase/go-genproto/dictybaseapis/api/jsonapi"
+	"github.com/dictyBase/go-genproto/dictybaseapis/identity"
+	"github.com/dictyBase/go-genproto/dictybaseapis/user"
+	"google.golang.org/grpc"
+	cli "gopkg.in/urfave/cli.v1"
+)
+
+func CreateIdentity(c *cli.Context) error {
+	uconn, err := grpc.Dial(
+		fmt.Sprintf("%s:%s", c.String("user-grpc-host"), c.String("user-grpc-port")),
+		grpc.WithInsecure(),
+	)
+	if err != nil {
+		return cli.NewExitError(
+			fmt.Sprintf("cannot connect to grpc server for user microservice %s", err),
+			2,
+		)
+	}
+	idconn, err := grpc.Dial(
+		fmt.Sprintf("%s:%s", c.String("identity-grpc-host"), c.String("identity-grpc-port")),
+		grpc.WithInsecure(),
+	)
+	if err != nil {
+		return cli.NewExitError(
+			fmt.Sprintf("cannot connect to grpc server for identity microservice %s", err),
+			2,
+		)
+	}
+	uclient := user.NewUserServiceClient(uconn)
+	res, err := uclient.ExistUser(context.Background(), &jsonapi.IdRequest{Id: c.Int64("user-id")})
+	if err != nil {
+		return cli.NewExitError(
+			fmt.Sprintf("error in retrieving user-id %d %s", c.Int64("user-id"), err),
+			2,
+		)
+	}
+	if !res.Exist {
+		return cli.NewExitError(
+			fmt.Sprintf("user-id %d does not exist", c.Int64("user-id")),
+			2,
+		)
+	}
+	idclient := identity.NewIdentityServiceClient(idconn)
+	idn, err := idclient.CreateIdentity(
+		context.Background(),
+		&identity.CreateIdentityReq{
+			Data: &identity.CreateIdentityReq_Data{
+				Type: "identities",
+				Attributes: &identity.NewIdentityAttributes{
+					Identifier: c.String("identifier"),
+					Provider:   c.String("provider"),
+					UserId:     c.Int64("user-id"),
+				},
+			},
+		})
+	if err != nil {
+		return cli.NewExitError(
+			fmt.Sprintf("error in creating identity %s", err),
+			2,
+		)
+	}
+	logger := getLogger(c)
+	logger.Infof(
+		"created identity %d with identifier %s provider %s and user id %d",
+		idn.Data.Id,
+		c.String("identifier"),
+		c.String("provider"),
+		c.Int64("user-id"),
+	)
+	return nil
+}
